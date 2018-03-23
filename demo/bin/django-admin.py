a96ea7b3de31e43952244355141145c7e7abf42b
Emad Aghayi <emad.aghayi@gmail.com>
2018-03-22 22:59:59 -0400

    Initial commit

---
+++ demo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/emadaghayi/Dropbox/PhD_Materials/Second_Semester/UI_Design_632/TechTalk/sourcecode/demo3/demo/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
